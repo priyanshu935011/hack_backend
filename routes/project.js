cf4876353b7e01780f2e73ca6aa0ca2267efe768
priyanshu935011 <95954702+priyanshu935011@users.noreply.github.com>
2025-08-31 17:56:06 +0530

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -12,8 +12,43 @@ router.post("/create", async (req, res) => {
       innovationPillar: innovationPillar,
       description: description,
       presentation: presentation,
+      status: 0,
     });
     res.status(200).json("Project registered successfully");
+  } catch {
+    res.status(400).json("Internal Error");
+  }
+});
+
+router.post("/fetch", async (req, res) => {
+  try {
+    const teamId = req.header("teamId");
+    const data = await Project.find({ teamId: teamId });
+
+    if (!data || data.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No projects found for this teamId" });
+    }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/update", async (req, res) => {
+  try {
+    const { projectId } = req.body;
+    const checkProject = await Project.findById({ _id: projectId });
+    if (checkProject) {
+      const projectUpdate = await Project.updateOne(
+        { _id: projectId },
+        { status: 1 }
+      );
+      res.status(200).json("Success");
+    }
   } catch {
     res.status(400).json("Internal Error");
   }
